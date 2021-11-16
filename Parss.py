f8e1c8e40489e6a85ceffaf8f6e758333b76a14a
Andrew-N27 <nandrii2027@gmail.com>
2021-11-16 15:43:01 +0200

    Edit function

--- Parss.py
+++ Parss.py
@@ -8,17 +8,26 @@ from bs4 import BeautifulSoup
 os.system('mkdir heros_csv')
 os.system('mkdir heros_json')
 
-URL = 'https://www.marvel.com/v1/pagination/grid_cards?offset=0&limit=36&entityType=character&sortField=title&sortDirection=asc'#'https://www.marvel.com/v1/pagination/grid_cards?offset=36&limit=36&entityType=character&sortField=title&sortDirection=asc'
+URL = 'https://www.marvel.com/v1/pagination/grid_cards?offset=0&limit=36&entityType=character&sortField=title&sortDirection=asc'
 HEADERS = {
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'
     }
 
-def get_json(url):
+
+def get_data_json(url):
     response = requests.get(url)
     return response.json()['data']['results']['data']
 
-data = get_json(URL)
+def get_hero_list(data, heros):
+    for i in range(0, 36):
+        hero = {}
+        hero['name'] = data[i]['link']['title']
+        hero['link'] = 'https://www.marvel.com' + data[i]['link']['link']
+        hero = hero | parse(hero['link'])
+        heros.append(hero)
+    return heros
+
 
 def parse(url):
     response = requests.get(url, headers = HEADERS)
@@ -32,28 +41,37 @@ def parse(url):
 
     return comps
 
-heros = []
 
-for i in range(0, 36):
-    hero = {}
-    hero['name'] = data[i]['link']['title']
-    hero['link'] = 'https://www.marvel.com' + data[i]['link']['link']
-    hero = hero | parse(hero['link'])
-    heros.append(hero)
+def file_write_csv(heros):
+    print('Створення CSV файлів')
 
-for hero in heros:
-    with open(f"heros_csv/{hero['name']}.csv", "w", newline="") as file:
-        writer = csv.writer(file)
+    for hero in heros:
+        with open(f"heros_csv/{hero['name']}.csv", "w", newline="") as file:
+            writer = csv.writer(file)
 
-        for key, value in hero.items():
-            writer.writerow([key, value])
+            for key, value in hero.items():
+                writer.writerow([key, value])
+    
+    print("CSV файли створені")
 
-print("CSV file - Done")
+def file_write_json(heros):
+    print('Створення JSON файлів')
 
-for hero in heros:
-    heros_json = json.dumps(hero)  
-    with open(f"heros_json/{hero['name']}.json", "w") as my_file:
-        my_file.write(heros_json)
+    for hero in heros:
+        heros_json = json.dumps(hero)  
+        with open(f"heros_json/{hero['name']}.json", "w") as my_file:
+            my_file.write(heros_json)
 
-print("JSON file - Done")
+    print('JSON файли створені')
 
+
+def main():
+    heros = []
+    dataset = get_data_json(URL)
+    heros = get_hero_list(dataset, heros)
+
+    file_write_csv(heros)
+    file_write_json(heros)
+
+if __name__ == '__main__':
+    main()
